e53681d78bceb7ea07216e84f93142fa14f29a79
Rhea Myers <rhea@myers.studio>
2010-10-23 22:17:33 +0100

    Have to use an application menu rather than a contextual menu for now. Must get the height of that dynamically, currently the window size is set using a fudge factor to cover it. UI works well now, very useful for analysing the performance of the code.

--- src/gtk.lisp
+++ src/gtk.lisp
@@ -16,16 +16,16 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Notes
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; This file breaks draw-something's "stand-alone Common Lisp" requirement
 ;; The rest of the code should still be stand-alone
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; TODO
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Allow the number of planes and (via...) the figure policies to be set
 
@@ -35,9 +35,9 @@
 ;; Might need to re-arrange form.lisp to support this
 ;; and/or add/remove wrappers dynamically
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Package setup
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require :asdf)
 (asdf:operate 'asdf:load-op :cl-gtk2-gtk)
@@ -51,33 +51,42 @@
 
 (in-package #:draw-something-gtk-ui)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Global variables
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defvar *drawing*)
+;; Just a dummy drawing initially
+(defvar *drawing* 
+  (make-instance 'draw-something::drawing
+		 :bounds (make-instance 'draw-something::rectangle
+					:width 400 :height 400)))
 (defvar *window*)
-(defvar *menu*)
 (defvar *drawing-area*)
 (defvar *draw-composition-points* t)
 (defvar *draw-skeletons* t)
 (defvar *draw-outlines* t)
 (defvar *draw-colours* nil)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Drawing creation
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmethod set-drawing ((d draw-something::drawing))
-  "Set the drawing and reconfigure the window to fit it"
-  (setf *drawing* d)
+(defmethod size-window-for-drawing (window (d draw-something::drawing))
   (gtk:within-main-loop
    (gtk:gtk-window-resize *window*
 			  (ceiling (draw-something::width
 				    (draw-something::bounds *drawing*)))
-			  (ceiling (draw-something::height
-				    (draw-something::bounds *drawing*))))
+			  ;;FIXME: Actually get menu bar height
+			  (+ 24
+			     (ceiling (draw-something::height
+				       (draw-something::bounds *drawing*)))))
    (gtk:widget-queue-draw *window*)))
+
+(defmethod set-drawing ((d draw-something::drawing))
+  "Set the drawing and reconfigure the window to fit it"
+  (assert *window*)
+  (setf *drawing* d)
+  (size-window-for-drawing *window* d))
 
 (defmethod make-new-drawing ()
   "Make a drawing and set the window to use it"
@@ -89,13 +98,13 @@
     (draw-something::colour-objects drawing draw-something::all-object-symbols)
     (set-drawing drawing)))
 
-(defmethod make-drawing-with-single-outline (width height skeleton-points policy)
+(defmethod make-drawing-with-single-outline (width height skeleton-points
+					     policy)
   "Make a drawing with a single plane/figure/form/skeleton/outline for testing"
   (let* ((drawing (make-instance 'draw-something::drawing
-				 :bounds (make-instance 'draw-something::rectangle
-							:x 0 :y 0 
-							:height height
-							:width width)
+				 :bounds (make-instance 
+					  'draw-something::rectangle
+					  :x 0 :y 0 :height height :width width)
 				 :composition-points skeleton-points))
 	 (figure (draw-something::make-figure-from-points skeleton-points))
 	 (plane (make-instance 'draw-something::plane
@@ -104,12 +113,12 @@
 			       :figure-count 1)))
     (setf (draw-something::planes drawing) (vector plane))
     (draw-something::draw-figure figure)
-    ;;(draw-something::colour-objects drawing draw-something::all-object-symbols)
+    (draw-something::colour-objects drawing draw-something::all-object-symbols)
     (set-drawing drawing)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Utilities for applying functions to forms
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Move these to the main package, they're useful
 
@@ -128,9 +137,9 @@
   (loop for p across (draw-something::planes d)
        do (apply-plane-forms p fun)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Drawing the drawing in the drawing-area
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defmethod draw-background ()
   "Erase the drawing area, setting the background colour if colouring"
@@ -183,9 +192,7 @@
 	     (draw-something::y (draw-something::from line)))
     (line-to (draw-something::x (draw-something::to line))
 	     (draw-something::y (draw-something::to line))))
-  (set-source-rgb (draw-something::random-range 0.1 0.4)
-		  (draw-something::random-range 0.1 0.4)
-		  (draw-something::random-range 0.1 0.4))
+  (set-source-rgb 0 0 0)
   (stroke))
 
 (defmethod draw-outlines ()
@@ -194,12 +201,12 @@
 
 (defmethod draw-form-colour ((f draw-something::form))
   "Colour in the outline of the form"
-  (set-line-width 1)
-  (draw-something::do-poly-lines (line  (draw-something::outline f))
-    (move-to (draw-something::x (draw-something::from line))
-	     (draw-something::y (draw-something::from line)))
-    (line-to (draw-something::x (draw-something::to line))
-	     (draw-something::y (draw-something::to line))))
+  (let ((outline (draw-something::points (draw-something::outline f))))
+    (move-to (draw-something::x (aref outline 0))
+	     (draw-something::y (aref outline 0)))
+    (loop for i from 1 below (length outline)
+	 do (line-to (draw-something::x (aref outline i))
+		     (draw-something::y (aref outline i)))))
   (multiple-value-bind (r g b) 
       (draw-something::hsb-to-rgb (draw-something::fill-colour f))
     (set-source-rgb r g b))
@@ -209,9 +216,9 @@
   "Colour in the outlines of all the forms in the drawing"
   (apply-drawing-forms *drawing* #'draw-form-colour))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; The Gtk+ user interface
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun handle-expose-event (widget)
   "Redraw the drawing in the drawing area"
@@ -228,17 +235,17 @@
 	(draw-outlines))
       nil)))
 
-(defmethod connect-toggled-signal (menu-item global)
+(defmacro connect-toggled-signal (menu-item global)
   "Set it so that toggling the menu item toggles the global variable"
-  (gobject:connect-signal menu-item "toggled"
-			  (lambda (widget userdata)
-			    (declare (ignore userdata))
-			    (setf global (gtk:check-menu-item-active widget))
+  `(gobject:connect-signal ,menu-item "toggled"
+			  (lambda (widget)
+			    (setf ,global (gtk:check-menu-item-active widget))
 			    (gtk:widget-queue-draw *window*))))
 
-(defmethod make-contextual-menu ()
-  "Make the contextual menu for the drawing area"
-  (let ((menu (make-instance 'gtk::menu))
+(defmethod make-menu ()
+  "Make the menu for the window"
+  (let ((menu (make-instance 'gtk::menu-item :label "Drawing"))
+	(submenu (make-instance 'gtk:menu))
 	(new-drawing (make-instance 'gtk:menu-item :label "New Drawing"))
 	(separator (make-instance 'gtk:separator-menu-item))
 	(composition (make-instance 'gtk:check-menu-item
@@ -247,8 +254,8 @@
 	(outline (make-instance 'gtk:check-menu-item :label "Outline"))
 	(colour (make-instance 'gtk:check-menu-item :label "Colour")))
     (gobject:connect-signal new-drawing "activate"
-			    (lambda (widget userdata)
-			      (declare (ignore widget) (ignore userdata))
+			    (lambda (widget)
+			      (declare (ignore widget))
 			      (make-new-drawing)))
     (setf (gtk:check-menu-item-active composition) *draw-composition-points*)
     (setf (gtk:check-menu-item-active skeleton) *draw-skeletons*)
@@ -258,48 +265,47 @@
     (connect-toggled-signal skeleton *draw-skeletons*)
     (connect-toggled-signal outline *draw-outlines*)
     (connect-toggled-signal colour *draw-colours*)
-    (gtk:menu-shell-append menu new-drawing)
-    (gtk:menu-shell-append menu separator)
-    (gtk:menu-shell-append menu composition)
-    (gtk:menu-shell-append menu skeleton)
-    (gtk:menu-shell-append menu outline)
-    (gtk:menu-shell-append menu colour)
+    (gtk:menu-shell-append submenu new-drawing)
+    (gtk:menu-shell-append submenu separator)
+    (gtk:menu-shell-append submenu composition)
+    (gtk:menu-shell-append submenu skeleton)
+    (gtk:menu-shell-append submenu outline)
+    (gtk:menu-shell-append submenu colour)
+    (setf (gtk:menu-item-submenu menu) submenu)
     menu))
 
 (defmethod make-window ()
   "Make the main UI window, complete with drawing area and contextual menu"
   (gtk:within-main-loop
-   (setf *window* (make-instance 'gtk:gtk-window
-				 :type :toplevel
-				 :window-position :center
-				 :title "draw-something"
-				 :default-width 200
-				 :default-height 200))
-   (setf *drawing-area* (make-instance 'gtk:drawing-area))
-   (gobject:connect-signal *drawing-area* 
-			   "expose-event"
-			   (lambda (widget event)
-			     (declare (ignore event))
-			     (handle-expose-event widget)))
-   (setf *menu* (make-contextual-menu))
-   (gobject:connect-signal *drawing-area* 
-			   "button-release-event"
-			   (lambda (widget event)
-			     (declare (ignore widget) (ignore event))
-			     (when t ;FIXME: (= (gtk:button event) 3)
-			       (gtk:menu-popup *menu* :button 3)
-			       ;; ??? What does this map to in the gtk C api?
-			       ;;                  (gtk:show-all *menu*)
-			       )))
-   (gtk:container-add *window* *drawing-area*)
-   (gtk:widget-show *window*)))
+   (let ((window (make-instance 'gtk:gtk-window
+				:type :toplevel
+				:window-position :center
+				:title "draw-something"
+				:default-width 200
+				:default-height 200))
+	 (menu-bar (make-instance 'gtk:menu-bar))
+	 (drawing-area (make-instance 'gtk:drawing-area))
+	 (v-box (make-instance 'gtk:v-box)))
+     (if *drawing*
+	 (size-window-for-drawing *window* *drawing*))
+     (gtk:container-add window v-box)
+     (gtk:box-pack-start v-box menu-bar :expand nil)
+     (gtk:widget-show menu-bar)
+     (gtk:menu-shell-append menu-bar (make-menu))
+     (gtk:box-pack-start v-box drawing-area)
+     (gobject:connect-signal drawing-area
+			     "expose-event"
+			     (lambda (widget event)
+			       (declare (ignore event))
+			       (handle-expose-event widget)))
+     (gtk:widget-show window)
+     (setf *window* window)
+     (setf *drawing-area* drawing-area))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Main flow of control
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Make the window
 (setf *window* (make-window))
 
-;; Dummy drawing initially
-(set-drawing (draw-something::make-drawing))
