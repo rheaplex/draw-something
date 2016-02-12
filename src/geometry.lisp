3cb2ab5d32997448a2b5179ac82dd72a2b6895e8
Rhea Myers <rhea@myers.studio>
2016-02-11 22:42:51 -0800

    Move tests to Prove. Remove old run system files. Convert some defmethods to defuns to start working on implicit generic function warnings.

--- src/geometry.lisp
+++ src/geometry.lisp
@@ -24,13 +24,13 @@
 (defconstant %radians-to-degrees (/ radian 360.0)
   "The value to multiple radians by to get degrees.")
 
-(defmethod radians-to-degrees (radians)
+(defun radians-to-degrees (radians)
   "Convert a value in radians to a huamn-readable value in degrees. :-)"
   (/ radians %radians-to-degrees))
 
 (defconstant radians-to-t-ratio (/ 1.0 (* pi 2.0)))
 
-(defmethod radians-to-t (r)
+(defun radians-to-t (r)
 	"Convert the value in radians to a value from 0.0 to 1.0"
 	(* r radians-to-t-ratio))
 
