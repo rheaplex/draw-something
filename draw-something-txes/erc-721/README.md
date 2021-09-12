2f1161fbd64a26cac8ba20f33c3725d5dc157821
Rhea Myers <rhea@myers.studio>
2021-09-12 15:24:57 -0700

    Deploying ERC721 contracct, minting tokens, and using their hashes as drawing skeletons now works.

---
+++ draw-something-txes/erc-721/README.md
@@ -0,0 +1,37 @@
+draw-something (txes)
+=====================
+
+"draw-something, but on the blockchain"
+
+You'll need node, npm, npx and hardhat installed.
+
+
+Testing
+=======
+
+Running Tests
+-------------
+
+Run:
+
+``` shell
+npx run hardhat test
+```
+
+Local Testing Of Scripts
+------------------------
+
+Install and run ganache-cli in one terminal window:
+
+``` shell
+npm install -g ganache-cli
+ganache-cli --port 5485
+```
+
+Run the scripts in another teminal window:
+
+``` shell
+npx hardhat run scripts/deploy.js --network ganache
+npx hardhat run scripts/createTokens.js --network ganache
+npx hardhat run scripts/getTokenCreateEvents.js --network ganache > ../tx-hashes
+```
---
+++ draw-something-txes/erc-721/scripts/createTokens.js
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+const fs = require('fs');
+
+async function main() {
+  const { abi } = JSON.parse(
+    fs.readFileSync(
+      `${__dirname}/../artifacts/contracts/DrawSomethingTxes.sol/DrawSomethingTxes.json`
+    ).toString());
+  const { address } = JSON.parse(fs.readFileSync(
+      `${__dirname}/../addresses/DrawSomethingTxes-${network.name}.json`
+  ).toString());
+ 
+  const [ signer ] = await ethers.getSigners();
+  
+  const ds_rw = new ethers.Contract(address, abi, signer);
+
+  const MAX_TOKENS = await ds_rw.MAX_TOKENS();
+  
+  let count = await ds_rw.totalSupply();
+  console.log(`${count} of ${MAX_TOKENS} minted on $(network.name).`);
+  while (count.lt(MAX_TOKENS)) {
+    const tx = await ds_rw.mintToken();
+    // We do not want to use the hash or mint the next token
+    // until this one is successfully on the chain.
+    const receipt = await tx.wait();
+    // 0 is error, a la UNIX
+    if (receipt.status === 0) {
+      throw new Error(
+        `Minting transaction for token #${count} failed. Sadface.`
+      );
+    }
+    count = await ds_rw.totalSupply();
+    console.log(`${count}: ${tx.hash}`);
+  }
+  console.log("All tokens minted.");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
